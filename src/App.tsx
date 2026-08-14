06e32fa99b633a048c2ef2eb58b430df53a3cbe5
Deep Akabari <deep.akabari@tatvasoft.com>
2026-08-14 12:02:53 +0530

    feat: 🎸 implement context to toggle theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import Board from "./components/Board"
-import './App.css'
+import "./App.css"
+import { useTheme } from "./context/ThemeContext"
 
 function App() {
+  const { theme, toggleTheme } = useTheme()
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
       <h1>TaskFlow</h1>
+      <button onClick={toggleTheme}>Toggle Theme {theme}</button>
       <Board />
     </div>
   )
